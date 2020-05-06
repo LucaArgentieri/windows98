5c68015f26f02a365ee520a1c54e0310a675dfa0
LucaArgentieri <50273216+LucaArgentieri@users.noreply.github.com>
2020-05-07 00:01:21 +0200

    Update 0.0.5

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
 // Orario
-
-function updateClock() {
-    let today = new Date();
-    let time = today.getHours() + ":" + today.getMinutes()
-    document.getElementById('orario').innerHTML=time
-
+function updateTime(){
+    var currentTime = new Date()
+    var hours = currentTime.getHours()
+    var minutes = currentTime.getMinutes()
+    if (minutes < 10){
+        minutes = "0" + minutes
+    }
+    var t_str = hours + ":" + minutes + " ";
+    if(hours > 11){
+        t_str += "PM";
+    } else {
+        t_str += "AM";
+    }
+    document.getElementById('orario').innerHTML = t_str;
 }
+setInterval(updateTime, 1000);
 
-setInterval(updateClock, 1000);
 
+
+
+
+
+
+
+
+
+
+
+// Modal #1
+let close1 = document.getElementById('close1');
+let risorse = document.getElementById('risorse')
+let risorseModal = document.getElementById("risorseModal");
+
+risorse.addEventListener('click', evt => {
+    evt.preventDefault()
+    risorseModal.style.display = "block";
+
+})
+
+close1.addEventListener('click', evt => {
+if (risorseModal.style.display = "block") {
+    risorseModal.style.display = "none";
+  }
+})
+
+// Modal #4
+let close2 = document.getElementById('close4');
+let cestino = document.getElementById('cestino')
+let cestinoModal = document.getElementById("cestinoModal");
+
+cestino.addEventListener('click', evt => {
+    evt.preventDefault()
+    cestinoModal.style.display = "block";
+
+})
+
+close4.addEventListener('click', evt => {
+    if (cestinoModal.style.display = "block") {
+        cestinoModal.style.display = "none";
+      }
+})
 
